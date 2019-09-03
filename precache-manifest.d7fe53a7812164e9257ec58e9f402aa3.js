a98ee0ad1019dd3f3e0ec9b19918878c81a0a91c
ssalmon <steven>
2019-09-03 15:01:16 +0100

    Updates

---
+++ precache-manifest.d7fe53a7812164e9257ec58e9f402aa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b7e9f2eef4bc41ea1a16924fbada1b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f10a0db60223b44cb33c",
+    "url": "/monsters-rolodex/static/css/main.fc1d9999.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "f10a0db60223b44cb33c",
+    "url": "/monsters-rolodex/static/js/main.bdb81d18.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
